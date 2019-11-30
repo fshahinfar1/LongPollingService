eda62dd4aa91b5ea2f93f2a33ab246dcdaedd98e
fshahinfar1 <fshahinfar1@gmail.com>
2019-11-30 22:11:07 +0330

    Add some component for showing feeds

--- src/front/long-polling/src/App.js
+++ src/front/long-polling/src/App.js
@@ -5,13 +5,57 @@ import {Link} from 'react-router-dom';
 import './App.css';
 
 
+function FancyButton(props) {
+	return (
+		<div className="fancy-button-container">
+			<button onClick={props.onClick}>{props.value}</button>
+		</div>
+	);
+}
+
+function Feed(props) {
+	return (
+		<div className='feed-container'>
+			<div className='feed-content'>
+				<h1>{props.title}</h1>
+				<p>{props.description}</p>
+			</div>
+			<div className='feed-footer'>
+				<FancyButton value='like'/>
+				<FancyButton value='comment'/>
+				<p className='feed-date'>1398-x-x</p>
+			</div>
+		</div>
+	);
+}
+
 function App() {
+	const feeds = [{
+		id: 0,
+		title: 'title',
+		description: 'test text'
+	},{
+		id: 1,
+		title: 'title2',
+		description: 'description 2'
+	}];
+
+	const feed_objs = feeds.map(function (obj) {
+		return <Feed
+						key={obj.id}
+						title={obj.title}
+						description={obj.description}
+					/>
+	});
   return (
 		<div>
 			<NavBar/>
 			<main>
 				<h1>Main</h1>
 				<Link to="/new-feed">wow</Link>
+				<div>
+					{feed_objs}
+				</div>
 			</main>
 			<footer>
 			</footer>
