e6d2d5f2a959e65ae3fb0d428677ce003f406b9b
amirsaeedr <amirsaeedranjbar@gmail.com>
2019-11-30 11:07:52 +0330

    Add WebMvcConfigurationSupport for swagger-ui

--- src/long-polling/src/main/java/ir/iust/computer/network/longpolling/configuration/SwaggerConfiguration.java
+++ src/long-polling/src/main/java/ir/iust/computer/network/longpolling/configuration/SwaggerConfiguration.java
@@ -2,6 +2,8 @@ package ir.iust.computer.network.longpolling.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -10,7 +12,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class SwaggerConfiguration {
+public class SwaggerConfiguration extends WebMvcConfigurationSupport {
     @Bean
     public Docket productApi() {
         return new Docket(DocumentationType.SWAGGER_2)
@@ -18,5 +20,13 @@ public class SwaggerConfiguration {
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build();
+    }
+
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
